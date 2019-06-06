a2962fe002c62373d5ce6241fec2f0176928253f
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2019-06-06 11:38:19 +0100

    Latest

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require('cors');
+const mysql = require('mysql');
+
+const config = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "fs_bnb"
+};
+const connection = mysql.createConnection(config);
+connection.connect();
 
 const app = express();
 
@@ -9,9 +20,32 @@ app.use(express.urlencoded({ extended: false }));
 
 app.post("/api/users", (req, res) => {
     // res.send("Yay!");
-    res.json({
-        message: "Yay!"
+    const user = req.body;
+    console.log(user);
+
+    // const userInsert = {
+    //     column_a: user.name,
+    //     columnB: user.email
+    // };
+
+    connection.query("INSERT INTO user SET ?", user, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({message: "Failed to insert"});
+        }
+
+        console.log(result);
+
+        var responseUser = {
+            id: result.insertId,
+            name: user.name,
+            email: user.email,
+            password: user.password
+        };
+
+        return res.status(200).json(responseUser);
     });
+
 });
 
 app.listen(3000, () => console.log("Listening on 3000"));
