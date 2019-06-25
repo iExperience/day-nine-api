4c0edce8e290042c784f88bb889ea12e995d97c2
Mikael von Ketelhodt <vonketelhodtm@gmail.com>
2019-06-25 10:17:24 +0100

    Ping test

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/ping", (req, res) => {
+  res.send("Pong");
+});
+
 app.post("/api/users", (req, res) => {
   // res.send("Yay!");
   const user = req.body;
